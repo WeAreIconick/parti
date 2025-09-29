e91e33d983b26135d36127bed73881be7dd7a429
George Stephanis <daljo628@gmail.com>
2025-09-29 12:34:51 -0400

    Update front-page demo content.

--- inc/starter-content.php
+++ inc/starter-content.php
@@ -24,23 +24,217 @@ function get_starter_content() {
 			'front' => array(
 				'post_type'    => 'page',
 				'post_title'   => esc_html_x( 'Front Page', 'Theme starter content', 'parti' ),
-				'post_content' => '<!-- wp:heading -->
-<h2 class="wp-block-heading">I am a heading!</h2>
+				'post_content' => '<!-- wp:columns {"verticalAlignment":null,"metadata":{"name":"Masthead"}} -->
+<div class="wp-block-columns"><!-- wp:column {"verticalAlignment":"center"} -->
+<div class="wp-block-column is-vertically-aligned-center"><!-- wp:group {"className":"is-style-blur-box","backgroundColor":"white-10","layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"left"}} -->
+<div class="wp-block-group is-style-blur-box has-white-10-background-color has-background"><!-- wp:paragraph {"style":{"elements":{"link":{"color":{"text":"var:preset|color|white-70"}}}},"textColor":"white-70"} -->
+<p class="has-white-70-color has-text-color has-link-color">#1 in Smart, Stylish Spaces</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:heading {"style":{"typography":{"fontStyle":"normal","fontWeight":"500"}},"fontSize":"xx-large"} -->
+<h2 class="wp-block-heading has-xx-large-font-size" style="font-style:normal;font-weight:500">Crafting spaces that match your style and needs</h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"style":{"elements":{"link":{"color":{"text":"var:preset|color|white-70"}}}},"textColor":"white-70","fontSize":"large"} -->
+<p class="has-white-70-color has-text-color has-link-color has-large-font-size">Building new or upgrading? We craft stylish, inspiring spaces that feel uniquely yours.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:buttons -->
+<div class="wp-block-buttons"><!-- wp:button -->
+<div class="wp-block-button"><a class="wp-block-button__link wp-element-button">Get in touch</a></div>
+<!-- /wp:button -->
+
+<!-- wp:button {"className":"is-style-outline"} -->
+<div class="wp-block-button is-style-outline"><a class="wp-block-button__link wp-element-button">View services</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:image {"sizeSlug":"large","className":"is-style-window"} -->
+<figure class="wp-block-image size-large is-style-window"><img src="https://framerusercontent.com/images/1P3nKNxZrYppBXWcED5K0c0KKk.png" alt=""/></figure>
+<!-- /wp:image --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
+
+<!-- wp:spacer -->
+<div style="height:100px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:group {"metadata":{"name":"Who We Are"},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:group {"className":"is-style-blur-box","backgroundColor":"white-10","layout":{"type":"constrained"}} -->
+<div class="wp-block-group is-style-blur-box has-white-10-background-color has-background"><!-- wp:paragraph {"align":"center"} -->
+<p class="has-text-align-center">Who we are</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:heading {"textAlign":"center","style":{"spacing":{"padding":{"right":"20%","left":"20%"}}},"fontSize":"x-large"} -->
+<h2 class="wp-block-heading has-text-align-center has-x-large-font-size" style="padding-right:20%;padding-left:20%">We’re a team of designers, architects, and builders turning spaces into works of art. From interiors to landscapes, we deliver lasting results tailored to your vision.</h2>
 <!-- /wp:heading -->
 
 <!-- wp:columns -->
-<div class="wp-block-columns"><!-- wp:column {"width":"66.66%"} -->
-<div class="wp-block-column" style="flex-basis:66.66%"><!-- wp:paragraph -->
-<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam facilisis metus sit amet consequat dignissim. Etiam pretium nisi orci, quis congue enim mattis placerat. Integer vulputate lectus sit amet justo fringilla, non pretium justo condimentum. Aenean tempor ut nibh eget pellentesque. Mauris at condimentum arcu, ac gravida nulla. Etiam iaculis volutpat facilisis. Aliquam imperdiet vitae nulla vel fringilla.</p>
+<div class="wp-block-columns"><!-- wp:column -->
+<div class="wp-block-column"><!-- wp:heading {"style":{"typography":{"fontSize":"2.5rem"}}} -->
+<h2 class="wp-block-heading" style="font-size:2.5rem">15+</h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"style":{"elements":{"link":{"color":{"text":"var:preset|color|white-80"}}}},"textColor":"white-80","fontSize":"medium"} -->
+<p class="has-white-80-color has-text-color has-link-color has-medium-font-size">Years of experience</p>
 <!-- /wp:paragraph --></div>
 <!-- /wp:column -->
 
-<!-- wp:column {"width":"33.33%"} -->
-<div class="wp-block-column" style="flex-basis:33.33%"><!-- wp:paragraph -->
-<p>Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Curabitur quis sapien consequat, condimentum diam sit amet, lobortis quam. Donec tempor odio et turpis pulvinar, eget ornare justo bibendum. Maecenas ipsum nulla, semper a neque sit amet, finibus scelerisque ex. Sed fermentum, augue nec pretium semper, mi eros hendrerit nisi, vitae ultrices nibh lectus nec ex. Etiam turpis sem, tincidunt vel vehicula ac, ornare vitae magna. Aenean sollicitudin mi vitae lorem ultrices malesuada. Aliquam at massa et massa ornare sollicitudin. Maecenas porta diam sed odio bibendum, in fringilla sapien consectetur. Donec at convallis ante, quis pharetra quam. Sed vitae ante ut nulla scelerisque tempus. Nam at diam facilisis nibh eleifend ornare. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae;</p>
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:heading {"style":{"typography":{"fontSize":"2.5rem"}}} -->
+<h2 class="wp-block-heading" style="font-size:2.5rem">25+</h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"style":{"elements":{"link":{"color":{"text":"var:preset|color|white-80"}}}},"textColor":"white-80","fontSize":"medium"} -->
+<p class="has-white-80-color has-text-color has-link-color has-medium-font-size">Talented team members</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:heading {"style":{"typography":{"fontSize":"2.5rem"}}} -->
+<h2 class="wp-block-heading" style="font-size:2.5rem">100+</h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"style":{"elements":{"link":{"color":{"text":"var:preset|color|white-80"}}}},"textColor":"white-80","fontSize":"medium"} -->
+<p class="has-white-80-color has-text-color has-link-color has-medium-font-size">Completed projects</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:heading {"style":{"typography":{"fontSize":"2.5rem"}}} -->
+<h2 class="wp-block-heading" style="font-size:2.5rem">9+</h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"style":{"elements":{"link":{"color":{"text":"var:preset|color|white-80"}}}},"textColor":"white-80","fontSize":"medium"} -->
+<p class="has-white-80-color has-text-color has-link-color has-medium-font-size">Industry awards won</p>
 <!-- /wp:paragraph --></div>
 <!-- /wp:column --></div>
-<!-- /wp:columns -->',
+<!-- /wp:columns -->
+
+<!-- wp:spacer -->
+<div style="height:100px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:columns {"verticalAlignment":null} -->
+<div class="wp-block-columns"><!-- wp:column {"verticalAlignment":"stretch","style":{"border":{"radius":"2rem"},"spacing":{"padding":{"right":"var:preset|spacing|60","left":"var:preset|spacing|60","top":"var:preset|spacing|60","bottom":"var:preset|spacing|60"}}},"backgroundColor":"white-10"} -->
+<div class="wp-block-column is-vertically-aligned-stretch has-white-10-background-color has-background" style="border-radius:2rem;padding-top:var(--wp--preset--spacing--60);padding-right:var(--wp--preset--spacing--60);padding-bottom:var(--wp--preset--spacing--60);padding-left:var(--wp--preset--spacing--60)"><!-- wp:paragraph {"fontSize":"x-large"} -->
+<p class="has-x-large-font-size">We design with intention creating spaces that reflect your style, not just trends.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"style":{"elements":{"link":{"color":{"text":"var:preset|color|white-70"}}}},"textColor":"white-70","fontSize":"large"} -->
+<p class="has-white-70-color has-text-color has-link-color has-large-font-size">1: Balance of style and function</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"style":{"elements":{"link":{"color":{"text":"var:preset|color|white-70"}}}},"textColor":"white-70","fontSize":"large"} -->
+<p class="has-white-70-color has-text-color has-link-color has-large-font-size">2: Timeless results, built to last</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"style":{"elements":{"link":{"color":{"text":"var:preset|color|white-70"}}}},"textColor":"white-70","fontSize":"large"} -->
+<p class="has-white-70-color has-text-color has-link-color has-large-font-size">3: Thoughtful, detail driven process</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:image {"sizeSlug":"large","className":"is-style-rounded","style":{"border":{"radius":"2rem"}}} -->
+<figure class="wp-block-image size-large has-custom-border is-style-rounded"><img src="https://framerusercontent.com/images/2u4cXHIDnj0mj7MicCa0JS3EEVU.png" alt="" style="border-radius:2rem"/></figure>
+<!-- /wp:image --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"metadata":{"name":"Our Team"},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:group {"className":"is-style-blur-box","backgroundColor":"white-10","layout":{"type":"constrained"}} -->
+<div class="wp-block-group is-style-blur-box has-white-10-background-color has-background"><!-- wp:paragraph {"align":"center"} -->
+<p class="has-text-align-center">Our team</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:heading {"textAlign":"center","style":{"spacing":{"padding":{"right":"20%","left":"20%"}},"typography":{"fontStyle":"normal","fontWeight":"200","lineHeight":"1.1"}},"fontSize":"xx-large"} -->
+<h2 class="wp-block-heading has-text-align-center has-xx-large-font-size" style="padding-right:20%;padding-left:20%;font-style:normal;font-weight:200;line-height:1.1">The dedicated team behind the craft</h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"align":"center","style":{"elements":{"link":{"color":{"text":"var:preset|color|white-70"}}}},"textColor":"white-70"} -->
+<p class="has-text-align-center has-white-70-color has-text-color has-link-color">Our team unites designers, architects, and builders who care about every detail.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:columns {"metadata":{}} -->
+<div class="wp-block-columns"><!-- wp:column -->
+<div class="wp-block-column"><!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|30","bottom":"var:preset|spacing|30","left":"var:preset|spacing|30","right":"var:preset|spacing|30"}}},"backgroundColor":"white-10","layout":{"type":"constrained"}} -->
+<div class="wp-block-group has-white-10-background-color has-background" style="padding-top:var(--wp--preset--spacing--30);padding-right:var(--wp--preset--spacing--30);padding-bottom:var(--wp--preset--spacing--30);padding-left:var(--wp--preset--spacing--30)"><!-- wp:image {"aspectRatio":"1","scale":"cover","sizeSlug":"large","className":"is-style-window"} -->
+<figure class="wp-block-image size-large is-style-window"><img src="https://framerusercontent.com/images/ACUmnZA0OiCEkX7wewLf2CVP6s.jpeg" alt="" style="aspect-ratio:1;object-fit:cover"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:paragraph {"align":"center","fontSize":"large"} -->
+<p class="has-text-align-center has-large-font-size">Noah Bennett</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"align":"center","style":{"elements":{"link":{"color":{"text":"var:preset|color|white-70"}}},"spacing":{"margin":{"top":"0","bottom":"0"}}},"textColor":"white-70"} -->
+<p class="has-text-align-center has-white-70-color has-text-color has-link-color" style="margin-top:0;margin-bottom:0">Project Manager</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:social-links {"iconColor":"white-80","iconColorValue":"rgba(255,255,255,0.8)","align":"center","className":"is-style-logos-only","layout":{"type":"flex","justifyContent":"center"}} -->
+<ul class="wp-block-social-links aligncenter has-icon-color is-style-logos-only"><!-- wp:social-link {"service":"x"} /-->
+
+<!-- wp:social-link {"service":"instagram"} /-->
+
+<!-- wp:social-link {"service":"linkedin"} /--></ul>
+<!-- /wp:social-links --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|30","bottom":"var:preset|spacing|30","left":"var:preset|spacing|30","right":"var:preset|spacing|30"}}},"backgroundColor":"white-10","layout":{"type":"constrained"}} -->
+<div class="wp-block-group has-white-10-background-color has-background" style="padding-top:var(--wp--preset--spacing--30);padding-right:var(--wp--preset--spacing--30);padding-bottom:var(--wp--preset--spacing--30);padding-left:var(--wp--preset--spacing--30)"><!-- wp:image {"aspectRatio":"1","scale":"cover","sizeSlug":"large","className":"is-style-window"} -->
+<figure class="wp-block-image size-large is-style-window"><img src="https://framerusercontent.com/images/cozra1L3tecl0rX3VKd8v5u4NKE.png" style="aspect-ratio:1;object-fit:cover"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:paragraph {"align":"center","fontSize":"large"} -->
+<p class="has-text-align-center has-large-font-size">James Benjamin</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"align":"center","style":{"elements":{"link":{"color":{"text":"var:preset|color|white-70"}}},"spacing":{"margin":{"top":"0","bottom":"0"}}},"textColor":"white-70"} -->
+<p class="has-text-align-center has-white-70-color has-text-color has-link-color" style="margin-top:0;margin-bottom:0">Creative Director</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:social-links {"iconColor":"white-80","iconColorValue":"rgba(255,255,255,0.8)","align":"center","className":"is-style-logos-only","layout":{"type":"flex","justifyContent":"center"}} -->
+<ul class="wp-block-social-links aligncenter has-icon-color is-style-logos-only"><!-- wp:social-link {"service":"x"} /-->
+
+<!-- wp:social-link {"service":"instagram"} /-->
+
+<!-- wp:social-link {"service":"linkedin"} /--></ul>
+<!-- /wp:social-links --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|30","bottom":"var:preset|spacing|30","left":"var:preset|spacing|30","right":"var:preset|spacing|30"}}},"backgroundColor":"white-10","layout":{"type":"constrained"}} -->
+<div class="wp-block-group has-white-10-background-color has-background" style="padding-top:var(--wp--preset--spacing--30);padding-right:var(--wp--preset--spacing--30);padding-bottom:var(--wp--preset--spacing--30);padding-left:var(--wp--preset--spacing--30)"><!-- wp:image {"aspectRatio":"1","scale":"cover","sizeSlug":"large","className":"is-style-window"} -->
+<figure class="wp-block-image size-large is-style-window"><img src="https://framerusercontent.com/images/fGMII2DZxHfMG5shcSEK1AnhYvw.png" style="aspect-ratio:1;object-fit:cover"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:paragraph {"align":"center","fontSize":"large"} -->
+<p class="has-text-align-center has-large-font-size">Adam Reeds</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"align":"center","style":{"elements":{"link":{"color":{"text":"var:preset|color|white-70"}}},"spacing":{"margin":{"top":"0","bottom":"0"}}},"textColor":"white-70"} -->
+<p class="has-text-align-center has-white-70-color has-text-color has-link-color" style="margin-top:0;margin-bottom:0">Creative Director</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:social-links {"iconColor":"white-80","iconColorValue":"rgba(255,255,255,0.8)","align":"center","className":"is-style-logos-only","layout":{"type":"flex","justifyContent":"center"}} -->
+<ul class="wp-block-social-links aligncenter has-icon-color is-style-logos-only"><!-- wp:social-link {"service":"x"} /-->
+
+<!-- wp:social-link {"service":"instagram"} /-->
+
+<!-- wp:social-link {"service":"linkedin"} /--></ul>
+<!-- /wp:social-links --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
+<!-- /wp:group -->',
 			),
 			'about',
 			'contact',
